972ae048a22821e7302266aa8466c2c7c5122ff4
Ori <happynger@gmail.com>
2019-01-18 19:39:43 -0800

    Automated Update Jan 18 19:39:43

--- src/main.c
+++ src/main.c
@@ -6,13 +6,21 @@
 /*   By: ori <ori@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 19:27:44 by ori               #+#    #+#             */
-/*   Updated: 2019/01/18 19:28:07 by ori              ###   ########.fr       */
+/*   Updated: 2019/01/18 19:37:01 by ori              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_md5.h"
 
-int		main(int ac, char **av)
+static void	usage(void)
 {
+	ft_printf("{cyan}Usage: ft_ssl command [command opts] [command args]{}");
+	ft_error("\n");
+}
+
+int			main(int ac, char **av)
+{
+	if (ac < 2)
+		usage();
 	
 }
